a34827c996a5ecb880653b9f43f84e090b91ad2a
aasiria24 <amaalmoasiri@gmail.com>
2026-04-12 11:17:33 +0300

    Update 3-to_str.py

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Module that provides a function to convert a float to its string representation."""
+"""Module that provides a function to convert 
+a float to its string representation."""
 
 
 def to_str(n: float) -> str:
