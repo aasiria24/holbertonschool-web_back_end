a8dc766af9e50a269c0b58b57285e7d6c4c185fd
aasiria24 <amaalmoasiri@gmail.com>
2026-04-12 11:30:45 +0300

    Create 100-safe_first_element.py

---
+++ python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module that provides a function to safely
+return the first element of a sequence."""
+from typing import Sequence, Any, Optional
+
+
+def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+    """Return the first element of a sequence if it exists, otherwise return None."""
+    if lst:
+        return lst[0]
+    else:
+        return None
