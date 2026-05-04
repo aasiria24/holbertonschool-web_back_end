559ba9168c0130832ce955b31e613c028daa845f
aasiria24 <amaalmoasiri@gmail.com>
2026-05-04 02:19:54 +0000

    You used to attend a place like this at some point

---
+++ ES6_classes/0-classroom.js
@@ -0,0 +1,9 @@
+export default class ClassRoom {
+  /**
+   * ينشئ فصلاً دراسياً جديداً.
+   * @param {Number} maxStudentsSize - الحد الأقصى لعدد الطلاب.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
