932bff41e4466e74308bb7027d3287ec009e7ddb
aasiria24 <amaalmoasiri@gmail.com>
2026-05-04 02:27:20 +0000

    A Course, Getters, and Setters

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,44 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    // التحقق من البيانات عند إنشاء الكائن لأول مرة
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  // Getter & Setter لخاصية الاسم
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  // Getter & Setter لخاصية المدة (length)
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = value;
+  }
+
+  // Getter & Setter لخاصية الطلاب (students)
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    if (!Array.isArray(value) || !value.every((s) => typeof s === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = value;
+  }
+}
