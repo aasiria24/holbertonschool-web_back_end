4441f2ed92a3eb0ed44521563ca3e34709b7d6f1
aasiria24 <amaalmoasiri@gmail.com>
2026-05-10 11:04:26 +0300

    Create 9-groceries_list.js

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,15 @@
+/**
+ * Returns a map of groceries with specific items and quantities.
+ * @returns {Map<string, number>} A map containing grocery items.
+ */
+export default function groceriesList() {
+  const groceries = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
+
+  return new Map(groceries);
+}
